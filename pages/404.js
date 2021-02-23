7eb89bd6ea3ed914769087abdff78f1cd8bd12ce
Tommi Urtti <tommi.urtti@vincit.com>
2021-02-22 21:30:01 -0800

    Move page

--- pages/404.js
+++ pages/404.js
@@ -1,11 +1,13 @@
+// 404.js
+import Link from 'next/link';
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 
-export default function Home() {
+export default function FourOhFour() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>We have moved.</title>
+        <title>We have moved</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
